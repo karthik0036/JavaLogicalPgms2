0beb548f4cf719eb4bc22e577ca44baf7e039d53
Karthik <54240768+karthik0036@users.noreply.github.com>
2022-03-28 11:19:57 +0530

    Add files via upload

---
+++ CouponNum.java
@@ -0,0 +1,35 @@
+package com.bridge;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CouponNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.print("How Many Coupons You Want:");
+	        Scanner sc = new Scanner(System.in);
+	        int numberOfCoupons = sc.nextInt();
+	        int[] couponsCodes = new int[numberOfCoupons];
+	        int countOfCoupons = 0;
+	        System.out.print("Enter a Random Number:");
+	        int Number = sc.nextInt();
+	        while (countOfCoupons < numberOfCoupons) {
+	            int randomCoupons = (int) Math.floor(Math.random() * Number);
+	            boolean couponInCouponsCode = false;
+	            for (int i = 0; i < couponsCodes.length; i++) {
+	                if (randomCoupons == couponsCodes[i]) {
+	                    couponInCouponsCode = true;
+	                }
+	            }
+	            if (!(couponInCouponsCode)) {
+	                couponsCodes[countOfCoupons++] = randomCoupons;
+	            }
+	        }
+	        System.out.println("These are " + numberOfCoupons + " coupons you want: " + Arrays.toString(couponsCodes));
+	        
+	        sc.close();
+
+	}
+
+}
